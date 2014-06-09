ba37a3eab882e6377ac23fc5cab72d50b72a0e06
Jason Lu <lu.json@icloud.com>
2014-06-09 02:47:44 -0400

    hotfix

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,9 +4,18 @@
 # is considered to be the first unless any hosts have the primary
 # property set.  Don't declare `role :all`, it's a meta role.
 
-role :app, %w{deploy@example.com}
-role :web, %w{deploy@example.com}
-role :db,  %w{deploy@example.com}
+role :app, %w{deploy@onlynet.biz}
+set :deploy_to, "/srv/www/210_onlynet_prod_admin"
+set :static_shares, "/srv/www/202_onlynet_prod_static"
+set :thin_config_file, "./config/thin.yml"
+set :thin_pid_file, "tmp/pids/thin.pid"
+set :models_path, "/srv/www/201_onlynet_prod_models/current/models"
+#set :env, "production"
+set :env, "production"
+
+
+#role :web, %w{deploy@example.com}
+#role :db,  %w{deploy@example.com}
 
 
 # Extended Server Syntax
@@ -15,7 +24,7 @@ role :db,  %w{deploy@example.com}
 # server list. The second argument is a, or duck-types, Hash and is
 # used to set extended properties on the server.
 
-server 'example.com', user: 'deploy', roles: %w{web app}, my_property: :my_value
+#server 'example.com', user: 'deploy', roles: %w{web app}, my_property: :my_value
 
 
 # Custom SSH Options
